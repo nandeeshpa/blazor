cc46cfd9ff81fad81d71107fe2ace6b6c302e8ad
nandeeshpa <nandeeshna2006@gmail.com>
2025-07-02 12:40:09 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,14 +1,11 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using EventEase;
-using EventEase.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-// ✅ Add this line for state management
-builder.Services.AddSingleton<UserState>();
 
 await builder.Build().RunAsync();
